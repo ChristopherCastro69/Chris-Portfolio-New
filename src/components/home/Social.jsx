44301798e9308db6274c91dfc78ef08f72398dee
Topitops69 <topitops123@gmail.com>
2023-07-16 00:08:27 +0800

    First Commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home_social">
+        <a href="https://github.com/Topitops69" className="home_social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+        </a>
+
+        <a href="https://www.facebook.com/topitops05?mibextid=ZbWKwL    " className="home_social-icon" target="_blank">
+        <i class="uil uil-facebook-f"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/christopher-c-083561124/" className="home_social-icon" target="_blank">
+        <i class="uil uil-linkedin-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
