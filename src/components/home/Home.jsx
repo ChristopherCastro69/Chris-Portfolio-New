d2ca6438cc6d06f551a0dcc3896ec4976e7df6db
Topitops69 <topitops123@gmail.com>
2023-07-16 21:24:28 +0800

    Updated the about info

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import "./home.css"
 import Social from './Social'
 import Data from './Data'
+import ScrollDown from './ScrollDown'
 
 const Home = () => {
   return (
     <section className="home section" id="home">
-        <div className="home_container container gid">
+        <div className="home_container container grid">
             <div className="home_content grid">
                 <Social />
 
@@ -14,6 +15,7 @@ const Home = () => {
 
                 <Data />
             </div>
+            <ScrollDown />
         </div>
     </section>
   )
