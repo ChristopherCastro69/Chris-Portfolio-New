4c0aabdaa73f4f3a1a5400250b9c454bf25874bd
Christopher <topitops123@gmail.com>
2025-03-22 02:37:59 +0800

    Remove HeartAlert image asset and update project data in the portfolio, including new projects and enhanced descriptions for existing ones. Refactor project details to improve clarity and presentation.

--- src/components/project/Data.jsx
+++ src/components/project/Data.jsx
@@ -1,19 +1,92 @@
-import Blog2 from "../../assets/blog3.jpg";
 import App from "../../assets/app.png";
-import HeartAlert from "../../assets/HeartAlert.png";
-import Creds from "../../assets/e-cert.jpg";
+import HeartAlert from "../../assets/projects/HeartAlert.png";
+import LoanChecker from "../../assets/projects/LoanChecker.png";
+import PizzaPy from "../../assets/projects/PizzaPy.png";
+import AutoCert from "../../assets/projects/AutoCert.png";
+import TOHP from "../../assets/projects/tohp.png";
+import Cebby from "../../assets/projects/cebby.png";
 
 //========== PORTFOLIO DATA ==========
 export const projectsData = [
   {
     id: 1,
+    image: LoanChecker,
+    title: "Loan Checker AI",
+    desc: "AI-Powered Loan Eligibility Prediction with 85% Accuracy",
+    lang: "Python, Pandas, NumPy, Seaborn, Streamlit, Scikit-learn, Colab, Kaggle",
+    link: "https://loan-checker-ai.streamlit.app/",
+    repo: "https://github.com/ChristopherCastro69/Loan-Checker-AI",
+    category: "Projects",
+    services: [
+      "Designed and implemented an AI-powered loan eligibility prediction system using Python and Gaussian Naïve Bayes, achieving an 85% accuracy rate in financial decision-making.",
+
+      "Developed and deployed a machine learning pipeline, including data preprocessing, feature engineering, and model integration with Streamlit, ensuring a seamless user experience.",
+
+      "Authored a detailed technical blog on AI deployment, providing insights into real-world machine learning applications.",
+    ],
+  },
+  {
+    id: 2,
+    image: PizzaPy,
+    title: "PizzaPy Cebu Website",
+    desc: "Hacktoberfest 2024 Contribution: UX/UI & Backend Enhancements",
+    lang: "Python, Django, HTML, CSS, JavaScript, Node.js, Cloud Hosting ",
+    link: "https://www.pizzapy.ph/",
+    repo: "https://github.com/ChristopherCastro69/pizzapy-website",
+    category: "Contributions",
+    services: [
+      "Redesigned and enhanced the website’s landing page UX/UI, improving navigation, responsiveness, and overall user engagement.",
+
+      "Implemented dynamic image animations and cloud-hosted assets, optimizing website performance and visual appeal.",
+
+      "Collaborated in Hacktoberfest 2024, delivering high-quality contributions in frontend and backend development, reinforcing open-source community engagement.",
+    ],
+  },
+  {
+    id: 3,
+    image: AutoCert,
+    title: "AutoCert: Certificate Automation",
+    desc: "Automated Certificate Generation & Delivery for Tech Events",
+    lang: "Next.js, TypeScript, Supabase",
+    link: "https://autocert.vercel.app/",
+    repo: "https://github.com/ChristopherCastro69/autocert",
+    category: "Projects",
+    services: [
+      "Developed a scalable automation system that streamlined certificate generation and delivery, eliminating manual work and reducing processing time by 80%.",
+
+      "Successfully deployed in GDG Cebu DevFest 2024 and PizzaPy Cebu’s Accenture Python ML 2025, automating certificate distribution for hundreds of participants.",
+
+      "Integrated Next.js, TypeScript, and Supabase to ensure seamless, real-time processing with cloud storage for efficient document management.",
+    ],
+  },
+
+  {
+    id: 4,
+    image: TOHP,
+    title: "The One Hour Project",
+    desc: "Empowering Communities with Time-Based Volunteering",
+    lang: "Next.js, NestJS, TypeScript, Supabase, Google Cloud, PostgreSQL, Tailwind, ShadCN",
+    link: "https://theonehourproject.app/",
+    repo: "https://github.com/symphco",
+    category: "Contributions",
+    services: [
+      "Developed and optimized frontend and backend functionalities, ensuring a seamless user experience and system performance.",
+
+      "Engineered key client features using NestJS, Next.js, and Google Cloud, ensuring scalability and high availability for a time-based volunteering platform.",
+
+      "Collaborated closely with stakeholders to align technical execution with business goals, contributing to project management and client communications.",
+    ],
+  },
+
+  {
+    id: 5,
     image: HeartAlert,
     title: "Heart Disease Prediction",
     desc: "Predicting Heart Failure Risk: Supervised Learning with 89% Accuracy",
     lang: "Py, Pandas, NumPy, Matplotlib, Seaborn, Plotly, Streamlit, TF, Keras, Scikit-learn, Jupyter",
     link: "https://heartpredictiongpt.streamlit.app/",
     repo: "https://github.com/ChristopherCastro69/HeartAlert",
-    category: "data",
+    category: "Projects",
     services: [
       "Led a team of developers in the creation of a mobile-first web application using Python, which accurately identifies heart diseases and provides relevant causal information.",
 
@@ -23,50 +96,29 @@ export const projectsData = [
     ],
   },
   {
-    id: 2,
-    image: Blog2,
-    title: "Insights Blog",
-    desc: "Daily Insights for Inspiration, Growth and Living Your Best Day Today",
-    lang: "HTML/CSS, JS, TS, Node.js, Redux, Vite, Vercel, MUI, Java, Spring, Railway",
-    link: "https://insights-app-tawny.vercel.app/",
-    repo: "https://github.com/ChristopherCastro69/insights-app",
-    category: "web",
+    id: 6,
+    image: Cebby,
+    title: "Cebby",
+    desc: "Enhancing Community Engagement with Event Scheduling Features",
+    lang: "Next.js, TypeScript, Tailwind CSS, Supabase",
+    link: "https://www.getcebby.com/",
+    repo: "https://github.com/ChristopherCastro69/event-ni",
+    category: "Contributions",
     services: [
-      "Contributed on both backend and frontend development, utilizing HTML, Material UI (CSS), JavaScript, React framework, and Spring Boot to deliver MVC structured services for the project.",
+      "Implemented a calendar system, streamlining event scheduling and boosting community engagement.",
 
-      "Leveraged Java Spring Boot with REST integrations to allow users to comment on blog posts, improving user interaction and content engagement.",
-
-      "Integrated a new image feature using Cloudinary, efficiently and effectively incorporating image upload and management capabilities into the application",
-
-      "Utilized Railway for backend deployment and Vercel for frontend deployment, ensuring a seamless and efficient deployment process.",
+      "Enhanced the UI/UX design, improving accessibility and user navigation for a seamless experience.",
     ],
   },
   {
-    id: 3,
-    image: Creds,
-    title: "Creds",
-    desc: "E-Certificate Generator for Javascript Cebu Automated Testing Workshop Participants",
-    lang: "HTML/CSS, JS, TS, Node.js, Vercel",
-    link: "https://creds-workshop.vercel.app/",
-    repo: "https://github.com/ChristopherCastro69/Creds",
-    category: "web",
-    services: [
-      "Guided participants through practical exercises and discussions on effective testing strategies using Javascript, Node, Playwright, TypeScript, Vite, and React.",
-
-      "Directed a team of volunteers, ensuring the success of the Javascript Cebu community event. Fostered collaboration and knowledge sharing within the JavaScript community.",
-
-      "Developed a website to distribute certificates for active participation, enhancing the recognition process and providing a convenient platform for participants.",
-    ],
-  },
-  {
-    id: 4,
+    id: 7,
     image: App,
     title: "Unshelf",
     desc: "The Ultimate Food Saver Ecommerce App for a Sustainable Future",
     lang: "Java, Kotlin, Android Studio, Maven, Firebase, Jetpack Compose, Google API",
     link: "https://unshelf-app-website-j6moubam9-christophercastro69s-projects.vercel.app/",
     repo: "https://github.com/ChristopherCastro69/UnShelf",
-    category: "app",
+    category: "Projects",
     services: [
       "Contributed to the design and backend implementation of an e-commerce application, focusing on the seller UI and Seller CRUD operations within the Firestore database to facilitate the buying and selling of food waste.",
 
